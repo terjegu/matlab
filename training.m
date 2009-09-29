2b4a03f09543cfc3c5fc3bddb2c5dfc868ab2a04
Terje Gundersen <terjegu@stud.ntnu.no>
2009-09-29 16:36:49 +0200

    calculating matrices

--- training.m
+++ training.m
@@ -1,6 +1,5 @@
 %% SCRIPT FILE
 % Terje Gundersen 15.09.2009
-% Last updated 22.09.2009
 close all;
 clear all;
 
@@ -13,7 +12,7 @@ window_size = 20e-3; % 20ms
 inc = f_s * window_size; % samples per frame
 N_loop = floor(length(x)/inc);
 
-F = [];
+F = zeros(N_loop,inc);
 for i=1:N_loop
    F(i,:) = x(1+inc*(i-1):inc*i); 
 end
@@ -31,13 +30,6 @@ lpc_order = 12;
 X_lpc = lpc(F',lpc_order);
 
 
-% Subtract estimated signal from real signal
-error = zeros(m,n);
-for i=1:m
-    error(i,:) = filter([1 X_lpc(i,2:end)], 1, F(i,:));
-end
-
-
 %% Transformation LPC --> RC
 X_rc = zeros(m,lpc_order);
 for i=1:m
@@ -51,8 +43,18 @@ N_class = 5;
 P = zeros(m,N_class);
 
 gm_obj = gmdistribution.fit(X_rc,N_class,'CovType','diagonal'); % EM-alg
+
+
+%% Matrices
 P = posterior(gm_obj,X_rc); % Posterior probability
 
+x_mu_sigma = zeros(m,N_class);
+for i=1:m
+    for j=1:N_class
+        x_mu_sigma(i,j) = (X_rc(i,:)-gm_obj.mu(j,:))*(1./gm_obj.Sigma(1,:,j))';
+    end
+end
 
+D = P.*x_mu_sigma;
 
 %% Conversion Function

a4a8bee942570de82771838f4aa14c00a2fda78a
Terje Gundersen <terjegu@stud.ntnu.no>
2009-10-13 15:48:38 +0200

    working script and dtw

---
+++ makelsf.m
@@ -0,0 +1,55 @@
+function [X_lsf,Y_lsf,n,frame_length,X_lpc,F_x] = makelsf(x,y,f_s,p)
+% makelsf(x,y,f_s,p) makes a 20ms AR(12) analysis of x
+% and returns av matrix of LSF coefficients.
+
+
+window_size = 20e-3; % 20ms
+inc = f_s * window_size; % samples per frame
+
+
+% Source vector
+N_loop = floor(length(x)/inc);
+F_x = zeros(N_loop,inc);
+for i=1:N_loop
+   F_x(i,:) = x(1+inc*(i-1):inc*i); 
+end
+
+if N_loop < length(x)/inc
+   F_x(N_loop+1,:) = zeros(inc,1);
+   F_x(N_loop+1,1:(length(x)-inc*N_loop)) = x(inc*N_loop+1:end); 
+end
+
+
+% Target vector
+N_loop = floor(length(y)/inc);
+F_y = zeros(N_loop,inc);
+for i=1:N_loop
+   F_y(i,:) = y(1+inc*(i-1):inc*i); 
+end
+
+if N_loop < length(y)/inc
+   F_y(N_loop+1,:) = zeros(inc,1);
+   F_y(N_loop+1,1:(length(y)-inc*N_loop)) = y(inc*N_loop+1:end); 
+end
+
+
+% Dimensions
+[n,frame_length] = size(F_x);
+
+% Make AR(p) coefficients
+X_lpc = lpc(F_x',p);
+Y_lpc = lpc(F_y',p);
+
+
+% Transformation LPC --> LSF
+X_lsf = zeros(n,p);
+for i=1:n
+    X_lsf(i,:) = poly2lsf(X_lpc(i,:));
+end
+
+Y_lsf = zeros(n,p);
+for i=1:n
+    Y_lsf(i,:) = poly2lsf(Y_lpc(i,:));
+end
+
+end
--- training.m
+++ training.m
@@ -8,53 +8,9 @@ clear all;
 [y,fs_y]=wavread('data/t03s000228.wav');
 
 
-%% Split file in 20ms segments
-window_size = 20e-3; % 20ms
-inc = f_s * window_size; % samples per frame
-
-% Source vector
-N_loop = floor(length(x)/inc);
-F_x = zeros(N_loop,inc);
-for i=1:N_loop
-   F_x(i,:) = x(1+inc*(i-1):inc*i); 
-end
-
-if N_loop < length(x)/inc
-   F_x(N_loop+1,:) = zeros(inc,1);
-   F_x(N_loop+1,1:(length(x)-inc*N_loop)) = x(inc*N_loop+1:end); 
-end
-
-% Target vector
-N_loop = floor(length(y)/inc);
-F_y = zeros(N_loop,inc);
-for i=1:N_loop
-   F_y(i,:) = y(1+inc*(i-1):inc*i); 
-end
-
-if N_loop < length(y)/inc
-   F_y(N_loop+1,:) = zeros(inc,1);
-   F_y(N_loop+1,1:(length(y)-inc*N_loop)) = y(inc*N_loop+1:end); 
-end
-
-% Dimensions
-[n,frame_length] = size(F_x);
-
-
-%% Make AR(p) coefficients
+%% Split file in 20ms segments and convert to LSF vectors
 p = 12; % LPC order
-X_lpc = lpc(F_x',p);
-Y_lpc = lpc(F_y',p);
-
-
-%% Transformation LPC --> RC
-X_lsf = zeros(n,p);
-for i=1:n
-    X_lsf(i,:) = poly2lsf(X_lpc(i,:));
-end
-Y_lsf = zeros(n,p);
-for i=1:n
-    Y_lsf(i,:) = poly2lsf(Y_lpc(i,:));
-end
+[X_lsf,Y_lsf,n,frame_length] = makelsf(x,y,f_s,p);
 
 
 %% EM algorithm
